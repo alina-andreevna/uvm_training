9635fc8db5a1e2068f5b84a3d329f41f79362511
Alina Andreevna <ali-piratova@mail.ru>
2021-01-31 21:02:48 +0300

    FIR MODULE REFACTORING
    All defines moved into separte pakage.
    Code refactoring with using fir_if interface.

---
+++ sim/fir_param_pkg.svh
@@ -0,0 +1,8 @@
+package fir_param_pkg;
+
+  `define CLK_PERIOD 10ns
+
+  `define INPUT_WORD_SIZE 10
+  `define OUTPUT_WORD_SIZE 10
+
+endpackage: fir_param_pkg
--- src/fir_lowpass.sv
+++ src/fir_lowpass.sv
@@ -1,15 +1,8 @@
 // FIR_lowpass
 
-module fir_lowpass #(
-	INPUT_DATA_WIDTH = 10,
-	OUTPUT_DATA_WIDTH = 2 * INPUT_DATA_WIDTH + 2)
-	(
-	input wire clk_in,
-	input wire rst_in,
+`include "../sim/fir_param_pkg.svh"
 
-	input [INPUT_DATA_WIDTH-1 : 0] data_in,
-	output [OUTPUT_DATA_WIDTH-1 : 0] data_out
-	);
+ module fir_lowpass (fir_if.fir f);
 
 	localparam order = 8;
 
@@ -23,17 +16,17 @@ module fir_lowpass #(
 	localparam b7 = 8'd17;
 	localparam b8 = 8'd7;
 
-	reg [INPUT_DATA_WIDTH-1 : 0] x[1 : order];
+	reg [`INPUT_WORD_SIZE-1 : 0] x[1 : order];
 	integer k;
 
-	assign data_out = b0 * data_in + b1 * x[1] + b2 * x[2] + b3 * x[3] + 
+	assign f.data_out = b0 * f.data_in + b1 * x[1] + b2 * x[2] + b3 * x[3] + 
 						b4 * x[4] + b5 * x[5] + b6 * x[6] + b7 * x[7] + b8 * x[8];
 
-	always_ff @(posedge clk_in) begin : proc_x1
-		if(rst_in) begin
+	always_ff @(posedge f.clk_in) begin : proc_x1
+		if(f.rst_in) begin
 			for (k=1; k<=order; k++) x[k] <= 0;
 		end else begin
-			x[1] <= data_in;
+			x[1] <= f.data_in;
 			for (k=2; k<=order; k++) x[k] <= x[k-1];
 		end
 	end
