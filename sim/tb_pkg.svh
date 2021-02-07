e0c6de8222fe916907793aed94e2f0a228866777
Alina Andreevna <ali-piratova@mail.ru>
2021-02-07 20:53:57 +0300

    UVM COMPONENTS INCLUDED INTO PACKAGE

--- sim/tb_pkg.svh
+++ sim/tb_pkg.svh
@@ -4,9 +4,9 @@ package tb_pkg;
   virtual interface fir_if global_fir_if;
 
   `include "uvm_macros.svh"
-  // `include "driver.svh"
-  // `include "scoreboard.svh"
-  // `include "printer.svh"
-  // `include "tester_env.svh"  
-  // `include "verbose_test.svh"
+  `include "driver.svh"
+  `include "scoreboard.svh"
+  `include "printer.svh"
+  `include "tester_env.svh"  
+  `include "verbose_test.svh"
 endpackage: tb_pkg
