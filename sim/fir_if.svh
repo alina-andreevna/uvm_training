f2e61639bfe3fb5744466ac9fffb6d6ca6f2801d
Alina Andreevna <ali-piratova@mail.ru>
2021-02-15 20:32:09 +0300

    INTERFACE UPDATED
    Ports direction actualized.
    Clock and reset functions removed.

--- sim/fir_if.svh
+++ sim/fir_if.svh
@@ -5,8 +5,8 @@ interface fir_if ;
     bit clk_in;
     bit rst_in;
 
-    wire [`INPUT_WORD_SIZE-1 : 0] data_in;
-    wire [`OUTPUT_WORD_SIZE-1 : 0] data_out;
+    reg [`INPUT_WORD_SIZE-1 : 0] data_in;
+    reg [`OUTPUT_WORD_SIZE-1 : 0] data_out;
 
     modport fir (
         input clk_in,
@@ -15,24 +15,9 @@ interface fir_if ;
         output data_out);
 
     modport tester(
-        input clk_in,
-        input rst_in,
+        output clk_in,
+        output rst_in,
         output data_in,
         input data_out);
-     
-     // generate clock and reset
-     
-     initial begin
-        clk_in = 0;
-        forever begin
-            #(`CLK_PERIOD) clk_in = ~clk_in;
-        end
-     end
-
-     initial begin
-        rst_in = 1;
-        #(`CLK_PERIOD * 5)   rst_in = 0;
-
-     end
      
 endinterface : fir_if
