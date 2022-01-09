b427c4b356d31c46c3776f0b2ee56a1422d89226
Alina Andreevna <ali-piratova@mail.ru>
2022-01-09 17:34:10 +0300

    Clean up compile list and execute file

--- compile.f
+++ compile.f
@@ -1,6 +1,13 @@
-sim/fir_if.svh
-sim/tb_pkg.svh
+src/pkg_fir.svh
 src/fir_lowpass.sv
-sim/tb_top_fir_lowpass.sv
 
-+incdir+./sim
+sim/gen_clk/if_clk_gen.sv
+sim/gen_clk/pkg_clk_gen.sv
+sim/gen_rst/if_rst_gen.sv
+sim/gen_rst/pkg_rst_gen.sv
+sim/gen_data/if_data_gen.sv
+sim/gen_data/pkg_data_gen.sv
+sim/gen_coeff/if_coeffs_gen.sv
+sim/gen_coeff/pkg_coeffs_gen.sv
+
+sim/tb/tb.sv
--- run_verbose.do
+++ run_verbose.do
@@ -1,8 +1,8 @@
 if [file exists "work"] {vdel -all -lib work}
 if ![file exists "rpt"] {mkdir rpt}
-# mkdir rpt
+
 vlib work
 vlog -f compile.f -l rpt/vlog.log
-# vsim -novopt -voptargs="+acc" tb +UVM_TESTNAME=tst_start -l rpt/vsim.log
-# do wave.do
-# run 500ns
+vsim -novopt -voptargs="+acc" tb +UVM_TESTNAME=tst_start -l rpt/vsim.log
+do sim/tests/wfs/dut.do
+run 500ns
